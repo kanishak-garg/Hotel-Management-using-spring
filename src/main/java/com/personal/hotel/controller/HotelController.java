be6d4cd5b735f8cfe9843daf70d04a5307c2a2f1
kanishak garg <kanishakgarg8778@gmail.com>
2024-01-03 23:36:02 +0530

    create service without DB

--- src/main/java/com/personal/hotel/controller/HotelController.java
+++ src/main/java/com/personal/hotel/controller/HotelController.java
@@ -1,6 +1,8 @@
 package com.personal.hotel.controller;
 
 import com.personal.hotel.model.Hotel;
+import com.personal.hotel.service.HotelService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HotelController {
 
+    @Autowired
+    HotelService hotelService;
+
     @PostMapping("/create")
     public void createHotel(Hotel hotel){
-
+        hotelService.createHotel(hotel);
     }
 }
--- src/main/java/com/personal/hotel/service/HotelService.java
+++ src/main/java/com/personal/hotel/service/HotelService.java
@@ -1,7 +1,22 @@
 package com.personal.hotel.service;
 
+import com.personal.hotel.model.Hotel;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class HotelService {
+
+    List<Hotel> hotelList = new ArrayList<>();
+    Map<String,Hotel> hotelMap = new HashMap<>();
+
+    public void createHotel(Hotel hotel){
+        hotelList.add(hotel);
+        hotelMap.put(hotel.getId(),hotel);
+    }
+
 }
