8e271056b5374ba20b8a29d51ace10666d50e651
kanishak garg <kanishakgarg8778@gmail.com>
2024-02-02 00:54:26 +0530

    add authorization on get all user

--- src/main/java/com/personal/hotel/controller/UserController.java
+++ src/main/java/com/personal/hotel/controller/UserController.java
@@ -3,6 +3,8 @@ package com.personal.hotel.controller;
 import com.personal.hotel.dto.UserDTO;
 import com.personal.hotel.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,11 +17,14 @@ public class UserController {
     UserService userService;
 
     @GetMapping("/getAll")
+    @ResponseStatus(HttpStatus.OK)
+    @PreAuthorize("hasRole('ADMIN')")
     public List<UserDTO> getAllUsers(){
         return userService.getAllUsers();
     }
 
     @PostMapping("/register")
+    @ResponseStatus(HttpStatus.CREATED)
     public void createUser(@RequestBody UserDTO userDTO){
         userService.createUser(userDTO);
     }
