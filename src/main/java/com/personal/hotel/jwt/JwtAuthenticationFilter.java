ee6029c207207edd552df4fe1d04891241aa564d
kanishak garg <kanishakgarg8778@gmail.com>
2024-02-05 16:44:59 +0530

    add Jwt Authentication

---
+++ src/main/java/com/personal/hotel/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,28 @@
+package com.personal.hotel.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        String requestHeader = request.getHeader("Authorization");
+        String username = null;
+        String token = null;
+        if(requestHeader!=null && requestHeader.startsWith("Bearer")){
+            token = requestHeader.substring(7);
+
+        }
+
+
+    }
+}
---
+++ src/main/java/com/personal/hotel/jwt/JwtAuthenticationHelper.java
@@ -0,0 +1,23 @@
+package com.personal.hotel.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtParser;
+import io.jsonwebtoken.Jwts;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtAuthenticationHelper {
+
+    private String secret="";
+    public String getUsernameFromToken(String token){
+        Claims claims = getClaimsFromToken(token);
+        String username = claims.getSubject();
+    }
+
+    public Claims getClaimsFromToken(String token){
+        JwtParser jwtParser = Jwts.parser().verifyWith(secret).build();
+        Claims claims = jwtParser.parseEncryptedClaims(token).getPayload();
+        return claims;
+    }
+
+}
