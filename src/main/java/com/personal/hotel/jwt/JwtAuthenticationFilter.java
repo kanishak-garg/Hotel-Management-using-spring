2ddd75afa84d77b6bce50b0a22586a4d80a2ddea
kanishak garg <kanishakgarg8778@gmail.com>
2024-02-05 19:07:11 +0530

    add security context

--- src/main/java/com/personal/hotel/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/personal/hotel/jwt/JwtAuthenticationFilter.java
@@ -4,6 +4,13 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -11,6 +18,12 @@ import java.io.IOException;
 
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    @Autowired
+    JwtAuthenticationHelper jwtHelper;
+
+    @Autowired
+    UserDetailsService userDetailsService;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
@@ -20,9 +33,18 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         String token = null;
         if(requestHeader!=null && requestHeader.startsWith("Bearer")){
             token = requestHeader.substring(7);
+            username = jwtHelper.getUsernameFromToken(token);
+            if(username!=null && SecurityContextHolder.getContext().getAuthentication() == null){
+                UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+                if(jwtHelper.isTokenExpired(token)){
+                    UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
+                            new UsernamePasswordAuthenticationToken(token,null,userDetails.getAuthorities());
+                    usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+                }
+            }
 
         }
-
-
+        filterChain.doFilter(request,response);
     }
 }
--- src/main/java/com/personal/hotel/jwt/JwtAuthenticationHelper.java
+++ src/main/java/com/personal/hotel/jwt/JwtAuthenticationHelper.java
@@ -5,10 +5,12 @@ import io.jsonwebtoken.JwtParser;
 import io.jsonwebtoken.Jwts;
 import org.springframework.stereotype.Component;
 
+import java.util.Date;
+
 @Component
 public class JwtAuthenticationHelper {
 
-    private String secret="";
+    private String secret="IamkanishakgargasoftwaredeveloperIamGoodwithSPRINGfbjsdbfjksedbfjs";
     public String getUsernameFromToken(String token){
         Claims claims = getClaimsFromToken(token);
         String username = claims.getSubject();
@@ -18,6 +20,12 @@ public class JwtAuthenticationHelper {
         JwtParser jwtParser = Jwts.parser().verifyWith(secret).build();
         Claims claims = jwtParser.parseEncryptedClaims(token).getPayload();
         return claims;
+    }
+
+    public Boolean isTokenExpired(String token){
+        Claims claims = getClaimsFromToken(token);
+        Date expDate = claims.getExpiration();
+        return expDate.before(new Date()); //before will tell exp date is not before the current date
     }
 
 }
